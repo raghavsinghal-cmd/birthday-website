d2d8e05e27ed5929d8d9b03a71c7df6e469961b0
raghavsinghal-cmd <raghavsinghal.amity@gmail.com>
2026-03-15 19:54:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ function confetti() {
     origin: { y: 0.6 }
   });
 }
+
+setInterval(createHeart, 500);
+
+function createHeart(){
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 20 + 10 + "px";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
